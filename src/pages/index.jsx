3590c90c2ba02b893ee289c1581a87a86ac09876
herloncosta <herlon36@gmail.com>
2022-09-29 19:58:40 -0300

    deleted:    pages/_app.js deleted:    pages/api/hello.js deleted:    pages/index.js modified:   src/pages/index.jsx deleted:    src/pages/jsx.jsx deleted:    src/pages/styled.jsx deleted:    src/styles/styled.module.css deleted:    styles/Home.module.css deleted:    styles/globals.css

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,5 @@
+import Header from "../components/Header";
+
 export default function Home() {
-	return (
-		<div>
-			<h1 className="text-blue">Fundamentos Next.js</h1>
-			<h2>Desenvolvendo o primeiro componente!</h2>
-		</div>
-	);
+	return <Header title="Fundamentos de Next.js e React.js" />;
 }
